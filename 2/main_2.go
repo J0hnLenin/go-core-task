bb3e751373ee1a5948d3ecdd7166a1f7dd672b88
J0hnLenin <ziziz123ziziz@gmail.com>
2026-01-27 22:24:38 +0500

    Начал делать задание 2

---
+++ 2/main_2.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func sliceExample(s []int) []int {
+	evenSlice := make([]int, 0, len(s))
+
+	for _, value := range s {
+		if value%2 == 0 {
+			evenSlice = append(evenSlice, value)
+		}
+	}
+	return evenSlice
+}
+
+func addElements(s []int, elment int) []int {
+	newSlice := make([]int, len(s)+1)
+	copy(newSlice, s)
+	newSlice[len(s)] = elment
+
+	return newSlice
+}
+
+func copySlice(s []int) []int {
+	newSlice := make([]int, len(s))
+	copy(newSlice, s)
+
+	return newSlice
+}
+
+func main() {
+	originalSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println("Слайс: ")
+	fmt.Println(originalSlice)
+	fmt.Println()
+	fmt.Println("Чётный слайс: ")
+	fmt.Println(sliceExample(originalSlice))
+	fmt.Println()
+
+	addition := 4
+	fmt.Printf("Слайс с добавлением элемента %d: \n", addition)
+	fmt.Println(addElements(originalSlice, addition))
+	fmt.Println()
+
+	fmt.Println("Копия слайса:")
+	fmt.Println(copySlice(originalSlice))
+	fmt.Println()
+	
+}
